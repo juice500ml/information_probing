bb36ee97f30a026a69c03f007822da84c95afee5
Kwanghee Choi <juice500ml@gmail.com>
2023-08-16 12:55:46 -0400

    implement test metrics

--- classification_probing.py
+++ classification_probing.py
@@ -3,7 +3,8 @@ from datetime import datetime
 from os import getcwd
 from pathlib import Path
 
-from pytorch_lightning import Trainer, seed_everything, loggers
+from pytorch_lightning import Trainer, seed_everything
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 from model import Wav2Vec2WithProbe
 from dataset import CommonPhoneDataModule
@@ -14,17 +15,20 @@ def main(args):
     model = Wav2Vec2WithProbe(num_classes=datamodule.num_classes, **args)
 
     seed_everything(seed=42, workers=True)
+    checkpoint_callback = ModelCheckpoint(monitor="val/loss")
+
     trainer = Trainer(
         accelerator=args["accelerator"],
         devices=args["devices"],
         fast_dev_run=args["fast_dev_run"],
         max_epochs=args["max_epochs"],
         val_check_interval=args["val_check_interval"],
+        callbacks=[checkpoint_callback],
         deterministic=True,
-        check_val_every_n_epoch=1,
-        default_root_dir=f"{getcwd()}/exps/{args['dataset_path']}/layer_index={args['layer_index']}/{datetime.today().isoformat()}"
+        default_root_dir=f"{getcwd()}/exps/{args['exp_name']}_{datetime.today().isoformat()}"
     )
     trainer.fit(model, datamodule=datamodule)
+    trainer.test(ckpt_path="best", datamodule=datamodule)
 
 
 if __name__ == "__main__":
@@ -37,7 +41,7 @@ if __name__ == "__main__":
     parser.add_argument("--num_workers", type=int, default=5, help="Number of multiprocessing workers for dataloading")
 
     # Model
-    parser.add_argument("--lr", type=float, default=5e-6, help="Learning rate for AdamW")
+    parser.add_argument("--lr", type=float, default=5e-5, help="Learning rate for AdamW")
     parser.add_argument("--weight_decay", type=float, default=0.0, help="Weight decay for AdamW")
     parser.add_argument("--tuning_type", choices=["linear", "finetune"], help="Type of training (linear: linear probing, finetune: fine-tuning)")
     parser.add_argument("--layer_index", type=int, help="Transformer layer index to use")
@@ -45,10 +49,11 @@ if __name__ == "__main__":
     parser.add_argument("--probe_hidden_dim", default=0, type=int, help="Hidden dimension for the MLP probes (Hewitt & Liang 2019)")
 
     # Trainer
+    parser.add_argument("--exp_name", type=str, default="test", help="Experiment name (Folder to store the results)")
     parser.add_argument("--accelerator",  default="gpu", help="gpu or cpu")
     parser.add_argument("--devices", default=1, help="# of gpus")
     parser.add_argument("--fast_dev_run", help="True if debug mode", default=False)
-    parser.add_argument("--max_epochs", type=int, default=5, help="Maximum epochs to train")
+    parser.add_argument("--max_epochs", type=int, default=10, help="Maximum epochs to train")
     parser.add_argument("--val_check_interval", type=float, default=1.0, help="Validation interval (1.0 = Once per epoch, 0.25 = 4 times per epoch)")
 
     args = parser.parse_args()
